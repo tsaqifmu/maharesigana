6a9b5d7b0d146a9f24ba29ced53f1306befc8106
tsaqifmu <mushoddaqt@gmail.com>
2024-07-18 19:44:22 +0700

    feat: integrate hero to strapi

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,39 +1,62 @@
 import Image from "next/image";
-import Container from "@/components/Container";
+import Link from "next/link";
 
-//* HERO COMPONENT SUDAH FIX
-const HeroContent = () => (
+import Container from "@/components/Container";
+import ExternalLink from "@/assets/icons/ExternalLink";
+import fetchBlog from "@/utils/fetchBlog";
+import { SimplifiedPost, simplifyResponse } from "@/utils/simplifyResponse";
+
+interface HeroContent {
+  title: string;
+  summary: string;
+  slug: string;
+}
+
+interface HeroImage {
+  media: {
+    url: string;
+    name: string;
+  };
+}
+
+const HeroContent = ({ title, summary, slug }: HeroContent) => (
   <Container className="absolute z-10 pt-14 font-inter text-white">
     <div className="flex h-full flex-col justify-center space-y-1 lg:space-y-3">
       <h2 className="heading2">Maharesigana</h2>
-      <h1 className="heading1 w-2/3">
-        Psychological First Aids : Kesehatan Mental Juga Memerlukan Bantuan
-      </h1>
-      <p className="paragraph w-2/3 lg:w-1/2">
-        Respon psikologis dari kejadian traumatis ini merupakan reaksi normal
-        dalam keadaan tidak normal. Meski demikian, hal ini harus mendapatkan
-        penanganan psikologis dengan segera seperti dukungan psikologis awal
-        (DPA) atau yang sering dikenal dengan……
-      </p>
+      <h1 className="heading1 w-2/3">{title}</h1>
+      <p className="paragraph line-clamp-4 w-2/3 lg:w-1/2">{summary}</p>
+      <Link
+        href={`/artikel/${slug}`}
+        className="flex w-fit items-center gap-x-2 rounded-full border-2 border-white px-3 py-1 text-lg font-medium text-white transition-all hover:border-primaryBlue hover:bg-primaryBlue"
+      >
+        <h4>Baca Selengkapnya</h4>
+        <ExternalLink />
+      </Link>
     </div>
   </Container>
 );
 
-const HeroImage = () => (
+const HeroImage = ({ media }: HeroImage) => (
   <Image
-    src={"/heroImage.png"}
-    alt="hero image"
+    src={`http://213.210.21.45:1337${media.url}`}
+    alt={media.name}
     width={1920}
     height={1080}
     className="h-screen object-cover object-center pt-14 brightness-50 lg:pt-[5.7rem]"
   />
 );
 
-const Hero = () => {
+const Hero = async () => {
+  const { data } = await fetchBlog(
+    "?populate[author][populate]=photo&populate=media&filters[headline][$eq]=true",
+  );
+  const blogsData: SimplifiedPost[] = simplifyResponse(data);
+  const blog = blogsData[0];
+  console.dir(blog, { depth: null });
   return (
     <section className="flexCenter relative">
-      <HeroContent />
-      <HeroImage />
+      <HeroContent title={blog.title} summary={blog.summary} slug={blog.slug} />
+      <HeroImage media={blog.media} />
     </section>
   );
 };
